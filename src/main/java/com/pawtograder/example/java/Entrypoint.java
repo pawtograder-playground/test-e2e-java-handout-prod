ad7e5b689c8c6456498174b86846c75d7ce47875
Jonathan Bell <jon@jonbell.net>
2025-08-03 09:40:44 -0400

    Initial commit

---
+++ src/main/java/com/pawtograder/example/java/Entrypoint.java
@@ -0,0 +1,28 @@
+package com.pawtograder.example.java;
+
+public class Entrypoint {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    /*
+     * This method takes two integers and returns their sum.
+     * 
+     * @param a the first integer
+     * @param b the second integer
+     * @return the sum of a and b
+     */
+    public int doMath(int a, int b) {
+        //TODO
+        return 0;
+    }
+
+    /**
+     * This method returns a message, "Hello, World!"
+     * @return
+     */
+    public String getMessage() {
+        //TODO
+        throw new UnsupportedOperationException("Not implemented");
+    }
+} 
